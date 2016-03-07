42f3c8fc6261b80dfba8302236e50544b3957b2c
Josh Ouyang <JoshOY789@gmail.com>
2016-03-07 15:46:08 +0800

    Complete position distribute.

--- dist/js/timeline.js
+++ dist/js/timeline.js
@@ -1,4 +1,6 @@
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -9,53 +11,96 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     _classCallCheck(this, Timenode);
 
     var d = new Date();
-    if (!year) {
-      year = d.getUTCYear();
-    }
-    if (!month) {
-      month = d.getMonth() + 1;
-    }
+    this.year = year || d.getUTCYear();
+    this.month = month || d.getMonth() + 1;
   };
 
   // 时间轴
 
 
-  var Timeline = function Timeline() {
-    var timenodes = arguments.length <= 0 || arguments[0] === undefined ? [] : arguments[0];
-    var width = arguments.length <= 1 || arguments[1] === undefined ? 560 : arguments[1];
+  var Timeline = function () {
+    function Timeline() {
+      var timenodes = arguments.length <= 0 || arguments[0] === undefined ? [] : arguments[0];
+      var width = arguments.length <= 1 || arguments[1] === undefined ? 760 : arguments[1];
+      var padding = arguments.length <= 2 || arguments[2] === undefined ? 140 : arguments[2];
 
-    _classCallCheck(this, Timeline);
+      _classCallCheck(this, Timeline);
 
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-      for (var _iterator = timenodes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-        var timenode = _step.value;
-      }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
-      try {
-        if (!_iteratorNormalCompletion && _iterator.return) {
-          _iterator.return();
-        }
-      } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
-        }
-      }
+      var self = this;
+      self.timenodes = timenodes;
+      self.width = width;
+      self.padding = padding;
+      self.monthRange = 0;
+      self.monthDistance = 0;
+      self.earliestMonth = 0;
     }
-  };
 
-  window.TimenodeImg = TimenodeImg;
-  window.TimeNode = TimeNode;
+    _createClass(Timeline, [{
+      key: 'addTimenode',
+      value: function addTimenode(timenode) {
+        var self = this;
+        self.timenodes = self.timenodes.concat(timenode);
+        self.timenodes.sort(function (a, b) {
+          if (a.year < b.year) {
+            return true;
+          } else if (a.year > b.year) {
+            return false;
+          } else /* a.year === b.year */{
+              return a.month < b.month;
+            }
+        });
+
+        if (self.timenodes.length === 1) {
+          return 0;
+        } else {
+          self.monthRange = self.timenodes[0].year * 12 + self.timenodes[0].month - (self.timenodes[self.timenodes.length - 1].year * 12 + self.timenodes[self.timenodes.length - 1].month);
+          self.monthDistance = (self.width - self.padding) / self.monthRange;
+          self.firstMonth = self.timenodes[0].year * 12 + self.timenodes[0].month;
+          self.earliestMonth = self.timenodes[self.timenodes.length - 1].year * 12 + self.timenodes[self.timenodes.length - 1].month;
+          return 0;
+        }
+      }
+    }, {
+      key: 'applyToDOM',
+      value: function applyToDOM(selector) {
+        var self = this;
+      }
+    }]);
+
+    return Timeline;
+  }();
+
+  window.Timenode = Timenode;
   window.Timeline = Timeline;
 
+  // window.demoTimeline = new Timeline();
+
   window.createTimeline = function (selector) {
-    console.log($(selector));
+    var width = arguments.length <= 1 || arguments[1] === undefined ? 760 : arguments[1];
+    var padding = arguments.length <= 2 || arguments[2] === undefined ? 140 : arguments[2];
+
+
+    var tl = new Timeline([], width, padding);
+
+    $(selector + ' > ul').children().map(function (idx, dom) {
+      var year = parseInt($(dom).attr('year'));
+      var month = parseInt($(dom).attr('month'));
+
+      var timenode = new Timenode(year, month);
+      tl.addTimenode(timenode);
+      console.log(tl);
+      return null;
+    });
+
+    $(selector + '> ul').children().map(function (idx, dom) {
+      var year = parseInt($(dom).attr('year'));
+      var month = parseInt($(dom).attr('month'));
+      var offset = (tl.padding / 2 + tl.monthDistance * (tl.firstMonth - (12 * year + month))).toString() + 'px';
+      $(dom).css('left', offset);
+      $(dom).text(year + '.' + month);
+    });
   };
 })();
+
+createTimeline('#timeline-demo');
 //# sourceMappingURL=../maps/timeline.js.map
--- src/js/timeline.js
+++ src/js/timeline.js
@@ -3,32 +3,93 @@
   class Timenode {
     constructor(year, month) {
       let d = new Date();
-      if (!year) {
-        year = d.getUTCYear();
-      }
-      if (!month) {
-        month = d.getMonth() + 1;
-      }
+      this.year = year || d.getUTCYear();
+      this.month = month || d.getMonth() + 1;
     }
   }
 
   // 时间轴
   class Timeline {
-    constructor(timenodes = [], width = 560) {
-      for (let timenode of timenodes) {
+    constructor(timenodes = [], width = 760, padding = 140) {
+      var self = this;
+      self.timenodes = timenodes;
+      self.width = width;
+      self.padding = padding;
+      self.monthRange = 0;
+      self.monthDistance = 0;
+      self.earliestMonth = 0;
+    }
 
+    addTimenode(timenode) {
+      var self = this;
+      self.timenodes = self.timenodes.concat(timenode);
+      self.timenodes.sort((a, b) => {
+        if (a.year < b.year) {
+          return true;
+        }
+        else if (a.year > b.year) {
+          return false;
+        }
+        else /* a.year === b.year */ {
+          return (a.month < b.month);
+        }
+      });
+
+      if (self.timenodes.length === 1) {
+        return 0;
+      }
+      else {
+        self.monthRange = (self.timenodes[0].year * 12 + self.timenodes[0].month)
+          - (self.timenodes[self.timenodes.length - 1].year * 12 + self.timenodes[self.timenodes.length - 1].month);
+        self.monthDistance = (self.width - self.padding) / self.monthRange;
+        self.firstMonth = self.timenodes[0].year * 12 + self.timenodes[0].month;
+        self.earliestMonth = self.timenodes[self.timenodes.length - 1].year * 12 + self.timenodes[self.timenodes.length - 1].month;
+        return 0;
       }
     }
+
+    applyToDOM(selector) {
+      var self = this;
+
+    }
+
   }
 
-
-
-  window.TimenodeImg = TimenodeImg;
-  window.TimeNode = TimeNode;
+  window.Timenode = Timenode;
   window.Timeline = Timeline;
 
-  window.createTimeline = (selector) => {
-    console.log($(selector));
+  // window.demoTimeline = new Timeline();
+
+  window.createTimeline = (selector, width=760, padding=140) => {
+
+    var tl = new Timeline([], width, padding);
+
+    $(selector + ' > ul').children().map(
+      (idx, dom) => {
+        let year = parseInt($(dom).attr('year'));
+        let month = parseInt($(dom).attr('month'));
+
+        var timenode = new Timenode(year, month);
+        tl.addTimenode(timenode);
+        console.log(tl);
+        return null;
+      }
+    );
+
+    $(selector + '> ul').children().map(
+      (idx, dom) => {
+        let year = parseInt($(dom).attr('year'));
+        let month = parseInt($(dom).attr('month'));
+        let offset = (tl.padding / 2 + tl.monthDistance * (tl.firstMonth - (12 * year + month) )).toString() + 'px';
+        $(dom).css('left', offset);
+        $(dom).text(year + '.' + month)
+      }
+    )
+
+
   };
 
 })();
+
+
+createTimeline('#timeline-demo');
